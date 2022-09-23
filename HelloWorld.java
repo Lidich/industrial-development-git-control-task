9e72afc4d01cba983a7596eb24475aeb8e2000a5
Кораблев Михаил Александрович <m.korablev@univ.priv>
2022-09-23 13:37:23 +0300

    merged new-config&new feature with conflict resolved

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!1"); // Display the string.
+        System.out.println("Hello World!1\n"); // Display the string.
 	System.out.println("New Feature!");
     }
 }
